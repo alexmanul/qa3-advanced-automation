757353087ddc7df25c12acdea892e4208eba60c6
alexmanul <aleksejs.marcena@gmail.com>
2020-02-02 15:16:13 +0200

    Added database filling script

--- src/test/java/SQLTest.java
+++ src/test/java/SQLTest.java
@@ -6,11 +6,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SQLTest {
 
+    String user = "test_user";
+    String password = "123456";
+
+    /**
+     * Task 1 – Simple validation
+     * First of all check the Database what it is inside it
+     * Create a test where we find agent with name Lucida and assert all his data (Check his id, name, city, phone number)
+     * Can be done through assert or SoftAsserts
+     */
     @Test
-    public void test1_find_and_assert_agent_data() throws SQLException {
-        String user = "test_user";
-        String password = "123456";
-        String query1 = "SELECT * FROM qa3.agents WHERE agents.agent_name = 'Lucida'";
+    public void test1() throws SQLException {
+        String query = "SELECT * FROM qa3.agents WHERE agents.agent_name = 'Lucida'";
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -19,8 +26,37 @@ public class SQLTest {
         }
         Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/qa3", user, password);
         Statement stmt = con.createStatement();
-        ResultSet resultSet = stmt.executeQuery(query1);
+        ResultSet resultSet = stmt.executeQuery(query);
         resultSet.next();
+        assertThat(resultSet.getString(1)).isEqualTo("A012");
+        assertThat(resultSet.getString(2)).isEqualTo("Lucida");
+        assertThat(resultSet.getString(3)).isEqualTo("San Jose");
+        assertThat(resultSet.getString(4)).isEqualTo("0.12");
+        assertThat(resultSet.getString(5)).isEqualTo("044-52981425");
+        assertThat(resultSet.getString(6)).isEqualTo("");
+        con.close();
+    }
+
+    /**
+     * Task 2 – Add/Remove data
+     * Create a test where we insert Agent – Test from Riga and PhoneNumber - 778-32556178
+     * Create a test where we update Agent – Mukesh –
+     * Change that he is from Bangladesh and PhoneNumber - 777-11111111
+     */
+    @Test
+    public void test2() throws SQLException {
+        String query = "SELECT * FROM qa3.agents WHERE agents.agent_name = 'Lucida'";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/qa3", user, password);
+        Statement stmt = con.createStatement();
+        ResultSet resultSet = stmt.executeQuery(query);
+        resultSet.next();
+        
         assertThat(resultSet.getString(1)).isEqualTo("A012");
         assertThat(resultSet.getString(2)).isEqualTo("Lucida");
         assertThat(resultSet.getString(3)).isEqualTo("San Jose");
